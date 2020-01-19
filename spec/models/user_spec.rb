6392a32ac61bd4b6c0e5f434695fc9fbb9fa4a01
rection <rectionxx@gmail.com>
2020-01-19 12:25:14 -0500

    Updated spec/user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it "Nickname e gore siralama" do
+    lindeman = Kullanici.create(isim: "Andy", nickname: "Lindeman")
+    chelimsky = Kullanici.create(isim: "David", nickname: "Chelimsky")
+
+    expect(Kullanici.order("nickname") == [chelimsky, lindeman])
+  end
 end
