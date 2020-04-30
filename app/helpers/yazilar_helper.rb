ba77761bdc501806d4be1a86006c853b4ce1cc98
Safa Bayar <rectionxx@gmail.com>
2020-04-30 02:19:05 -0400

    updated spaces

--- app/helpers/yazilar_helper.rb
+++ app/helpers/yazilar_helper.rb
@@ -2,6 +2,32 @@
 
 # YazilarHelper
 module YazilarHelper
+
+def markdown_to_html(markdown_str)
+  options = {
+    filter_html: true,
+    link_attributes: { rel: 'nofollow', target: '_blank' },
+    no_styles: true
+  }
+
+  extensions = {
+    autolink: true,
+    fenced_code_blocks: true,
+    footnotes: true,
+    highlight: true,
+    no_intra_emphasis: true,
+    quote: true,
+    space_after_headers: true,
+    strikethrough: true,
+    superscript: true,
+    tables: true
+  }
+
+  renderer = Redcarpet::Render::HTML.new(options)
+  markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+  markdown.render(markdown_str).html_safe
+end
   # def markdown(text)
   #   options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
   #   syntax_highlighter(Redcarpet.new(text, *options).to_html).html_safe
