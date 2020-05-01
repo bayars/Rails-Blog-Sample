bdfd2dcd9192962025b8fc7fb88c553e8c8122ec
Safa Bayar <rectionxx@gmail.com>
2020-05-01 11:02:35 -0400

    conf files of webpacker

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-// module.exports = {
-//     entry: './app/assets/javascripts/entry.js',
-//     output: {
-//         filename: './app/assets/javascripts/app.js',
-//         path: __dirname
-//     }
-// }
