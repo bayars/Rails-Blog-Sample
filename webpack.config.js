b6a01740ffc8a9a5297f3e777c6f84cffb2a1ffb
Safa Bayar <rectionxx@gmail.com>
2020-05-01 02:51:42 -0400

    removed webpacker

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-module.exports = {
-    entry: './app/assets/javascripts/entry.js',
-    output: {
-        filename: './app/assets/javascripts/app.js',
-        path: __dirname
-    }
-}
+// module.exports = {
+//     entry: './app/assets/javascripts/entry.js',
+//     output: {
+//         filename: './app/assets/javascripts/app.js',
+//         path: __dirname
+//     }
+// }
