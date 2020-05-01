9fd80688f8d384b332de3cdbaa5222724987be8a
rection <rectionxx@gmail.com>
2020-05-01 17:29:16 +0200

    updated rspec

--- test/controllers/hakkimda_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class HakkimdaControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get hakkimda_show_url
-    assert_response :success
-  end
-
-end
